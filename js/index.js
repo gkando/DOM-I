34ab0e5ca32dfc5fbd73fdd71dcdfcc72881946a
George Kando <gkando@gmail.com>
2019-05-13 14:04:42 -0500

    first commit
     On branch george-kando
     Changes to be committed:
    	new file:   .vscode/tasks.json
    	modified:   README.md
    	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Navigation
+
+  // for loop
+    // let nav = document.querySelectorAll("nav a");
+    // for (i = 0; i < nav.length; i++) {
+    //   nav[i].textContent = siteContent.nav["nav-item-" + i]
+    // }
+
+  // for each
+    document.querySelectorAll("nav a").forEach(function(el, i) {
+      el.textContent = siteContent.nav["nav-item-" + i]
+    })
+
+//cta
+  //text
+    document.querySelector('.cta-text h1').innerHTML = siteContent.cta["h1"].replace(/ /g, " <br> ");
+
+  //button
+    document.querySelector('.cta-text button').textContent = siteContent.cta["button"];
+  
+  //img
+    // let ctaimg = document.getElementById("cta-img");
+    document.getElementById('cta-img').setAttribute('src', siteContent["cta"]["img-src"]);
+
+// top content
+
+  let text = document.querySelectorAll('.text-content');
+  
+  //img
+
+    document.getElementById('middle-img').setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+  
+  //text
+
+    let mainTxt = siteContent["main-content"];
+    delete mainTxt["middle-img-src"];
+    let i = 0;
+
+    document.querySelectorAll(".text-content").forEach(function(el) {
+      el.firstElementChild.textContent = Object.values(mainTxt)[i]
+      el.lastElementChild.textContent = Object.values(mainTxt)[Number(i+1)]
+      i = i + 2;
+    })
+
+//contact
+    contact = document.querySelector('.contact');
+    for (let i = 0; i < contact.children.length; i++) {
+      contact.children[i].textContent = Object.values(siteContent["contact"])[i]
+    }
+
+    var contactBr =  document.querySelector('.contact p').innerHTML.replace("Street", 'Street <br />');
+    document.querySelector('.contact p').innerHTML = contactBr
+
+//footer
+  document.querySelector('footer p').textContent = siteContent.footer["copyright"];
